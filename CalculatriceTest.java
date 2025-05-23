fe12b34ac646f79847ebce9fa4fc3da265d4c2e6
EEEmryc <emrychumeau@gmail.com>
2025-05-23 10:57:20 +0200

    ajout classe test avec les différentes méthodes

---
+++ CalculatriceTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatriceTest {
+
+    private Calculatrice calcu;
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testMult() {
+    }
+
+    @Test
+    public void testDiv() {
+    }
+
+    @Test
+    public void testDivZero() {
+        }
+}
